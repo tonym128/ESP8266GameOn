44ae54b3c30cdf8b6804ffaf94b98e4a06b55390
Tony Mamacos <tmamacos@gmail.com>
2019-06-04 08:20:16 +0200

    Added header image and updates

--- game.cpp
+++ game.cpp
@@ -1,13 +1,3 @@
-// #define FPS 1
-#define ANALOG 1
-// #define AUDIO 1
-// #define DEBUG_OUTPUT
-
-// Define the screen you're using below, and define check the ESP32.h or ESP8266.h for pin settings
-// This has only been setup for the ESP32, but it can be backported
-#define SCREEN_ST7789
-// #define SCREEN_SSD1306
-
 #include "game.hpp"
 
 ScreenBuff screenBuff;
--- game.hpp
+++ game.hpp
@@ -1,6 +1,17 @@
 #ifndef _GAME_H
 #define _GAME_H
 
+// #define FPS 1
+// #define DEBUG_OUTPUT
+
+// #define ANALOG 1
+#define AUDIO 1
+
+// Define the screen you're using below, and define check the ESP32.h or ESP8266.h for pin settings
+// This has only been setup for the ESP32, but it can be backported
+#define SCREEN_ST7789
+// #define SCREEN_SSD1306
+
 #include "gameCommon.hpp"
 #include "myGame.hpp"
 
